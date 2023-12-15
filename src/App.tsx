fffc139660d01b8bb5c66eb5da8d9bc817a25aa4
Leo Carvalho <carvalho.csleo@gmail.com>
2023-12-14 21:10:12 -0300

    Limpeza geral do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import estilo from './App.module.css';
 
 function App() {
   return (
-    <div className={estilo.App}>
-      <header className={estilo['App-header']}>
-        <img src={logo} className={estilo['App-logo']} alt="logo" />
-        <p>
-          Olá mundo! Meu nome é Léo!
-        </p>
-        <a
-          className={estilo['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
