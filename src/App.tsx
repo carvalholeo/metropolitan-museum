7ef08090d4789622e6fb9535a811b9b7292ce336
Leo Carvalho <carvalho.csleo@gmail.com>
2023-12-21 21:34:10 -0300

    Cria a base de um Formulário padrão

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/Header';
 import Galeria from './components/Galeria';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div>
       <Header />
       <Galeria />
+      <Formulario />
     </div>
   );
 }
---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Formulario() {
+  return (
+    <div></div>
+  )
+}
+
+export default Formulario;
