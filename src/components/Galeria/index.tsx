4f0879cc3d3edd72e1cff0e76157964cc599756c
Leo Carvalho <carvalho.csleo@gmail.com>
2023-12-21 21:20:03 -0300

    Altera pra arraymap Implementa renderização condicional

--- src/components/Galeria/index.tsx
+++ src/components/Galeria/index.tsx
@@ -7,23 +7,30 @@ function Galeria() {
   const listaImages = [
     {
       caminho: 'https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      texto: 'Melhor descrição'
+      texto: 'Melhor descrição',
+      ehPraRenderizar: true
     },
     {
       caminho: 'https://images.unsplash.com/photo-1587691592099-24045742c181?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      texto: 'Melhor descrição'
+      texto: 'Melhor descrição',
+      ehPraRenderizar: true
     },
     {
       caminho: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      texto: 'Melhor descrição'
+      texto: 'Melhor descrição',
+      ehPraRenderizar: true
     },
   ];
   return (
     <Container>
       <div>
-        <div><Imagem caminho={listaImages[0].caminho} texto={listaImages[0].texto} /></div>
-        <div><Imagem caminho={listaImages[1].caminho} texto={listaImages[1].texto} /></div>
-        <div><Imagem caminho={listaImages[2].caminho} texto={listaImages[2].texto} /></div>
+        {listaImages.map(objeto => {
+          const key = Math.random() * 10;
+
+          return (objeto.ehPraRenderizar &&
+            <div key={key} ><Imagem caminho={objeto.caminho} texto={objeto.texto} /></div>
+          );
+        })}
       </div>
     </Container>
   );
