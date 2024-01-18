440493d7116f61cd0ca9040fc1e3c1bfa6542921
Leo Carvalho <carvalho.csleo@gmail.com>
2024-01-18 20:50:01 -0300

    Instala axios Explica APIs com React Prepara projeto para Metropolitan Museum

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import apiMetMuseum from '../../services/apis/api-met-museum';
 
 function Formulario() {
-  const [idade, setIdade] = useState(0);
+  const [idade, setIdade] = useState(27);
+
+  // equivalente ao componentWillMount
+  useEffect(function() {
+    apiMetMuseum.get('objects/436121')
+      .then(console.log)
+  }, []);
+
+  // equivalente à de atualização
+  useEffect(() => {
+    console.log('Executa a cada alteração de idade');
+    console.log(idade);
+  }, [idade]);
+
+
+  // equivalente ao componentWillUnmount
+  useEffect(() => {
+    return () => {
+      console.log('Executa quando o componente é desmontado');
+    }
+  }, []);
 
   function mudarIdade(evento: React.SyntheticEvent) {
     setIdade(idade + 1);
-    console.log(idade)
   }
 
   return (
