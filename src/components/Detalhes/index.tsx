d4baf5abfc3aaf5f6d3d5b1a21e180d7308bb3cf
Leo Carvalho <carvalho.csleo@gmail.com>
2024-02-22 21:04:15 -0300

    Cria tela de detalhes Cria rota para acessar a tela

--- src/components/Detalhes/index.tsx
+++ src/components/Detalhes/index.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 
 import Container from "../Container";
+import detalhes from './detalhes.json'
 
 function Detalhes() {
   return (
     <Container>
-      <table>
-        
+      <img src={detalhes.primaryImageSmall} alt={detalhes.title} />
+      <table border={1}>
+        <tr>
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th>Artista</th>
+              <th>Data de publicação</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{detalhes.title}</td>
+              <td>{detalhes.artistDisplayName}</td>
+              <td>{detalhes.objectEndDate}</td>
+            </tr>
+          </tbody>
+        </tr>
+        <tr>
+          <thead>
+            <tr>
+              <th>Dimensões do objeto</th>
+              <th>Departamento</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{detalhes.measurements[0].elementMeasurements.Height} x {detalhes.measurements[0].elementMeasurements.Width} cm</td>
+              <td>{detalhes.department}</td>
+            </tr>
+          </tbody>
+        </tr>
       </table>
     </Container>
   );
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -7,6 +7,7 @@ import {
 
 import ErrorBoundary from "../components/ErrorBoundary";
 import Galeria from "../components/Galeria";
+import Detalhes from "../components/Detalhes";
 
 function Router() {
   const minhasRotas = createBrowserRouter([
@@ -14,6 +15,10 @@ function Router() {
       path: "/",
       element: <Galeria />,
       errorElement: <ErrorBoundary />
+    },
+    {
+      path: '/detalhes',
+      element: <Detalhes />
     }
   ]);
 
